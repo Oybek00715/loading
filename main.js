a3b237ef61e8f447cc474b7703773cd02be350ac
unknown <beljan.beka123.123@gmail.com>
2023-02-08 14:11:20 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,97 @@
+
+
+
+
+
+
+
+// function f(){
+
+// function f1(){
+// $('.block1').css({"transition": "1s" ,"background": "red" })
+// $('.block3').css({"transition": "1s" ,"background": "white" })
+// }
+// setTimeout( f1, 1000)
+
+// function f2(){
+//     $('.block2').css({"transition": "1s" ,"background": "yellow" })
+//     $('.block1').css({"transition": "1s" ,"background": "white" })
+
+//     }
+// setTimeout( f2, 2000)   
+    
+// function f3(){
+//     $('.block3').css({"transition": "1s" ,"background": "green" })
+//     $('.block2').css({"transition": "1s" ,"background": "white" })
+//     }
+//     setTimeout( f3, 3000)
+// }
+
+
+// f()
+
+
+// setInterval(f , 3200)
+
+function f(){
+
+function f1(){
+    $('.box1').css({"background" :"white"})
+    $('.box8').css({"background" :"rgb(34, 34, 34)"})
+}
+f1()
+function f2(){
+    $('.box2').css({"background" :"white"})
+    $('.box1').css({"background" :"rgb(34, 34, 34)"})
+}
+
+setTimeout(f2 , 100)
+
+function f3(){
+    $('.box3').css({"background" :"white"})
+    $('.box2').css({"background" :"rgb(34, 34, 34)"})
+}
+
+setTimeout(f3 , 200)
+
+
+
+function f4(){
+    $('.box4').css({"background" :"white"})
+    $('.box3').css({"background" :"rgb(34, 34, 34)"})
+}
+
+setTimeout(f4 , 300)
+
+function f5(){
+    $('.box5').css({"background" :"white"})
+    $('.box4').css({"background" :"rgb(34, 34, 34)"})
+}
+
+setTimeout(f5 , 400)
+
+function f6(){
+    $('.box6').css({"background" :"white"})
+    $('.box5').css({"background" :"rgb(34, 34, 34)"})
+}
+
+setTimeout(f6 , 500)
+
+function f7(){
+    $('.box7').css({"background" :"white"})
+    $('.box6').css({"background" :"rgb(34, 34, 34)"})
+}
+
+setTimeout(f7 , 600)
+
+function f8(){
+    $('.box8').css({"background" :"white"})
+    $('.box7').css({"background" :"rgb(34, 34, 34)"})
+}
+
+setTimeout(f8 , 700)
+
+}
+
+
+setInterval( f , 800)       
